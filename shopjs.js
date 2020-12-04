502a97b5826562d9ea6541768ee2d251665b1fb5
igdownload <62426177+igdownload@users.noreply.github.com>
2020-12-04 14:28:49 +0530

    Added igNotePad Application
    A new GUI based application added in the shop.

--- shopjs.js
+++ shopjs.js
@@ -1,7 +1,7 @@
 var u = window.localStorage.getItem('name');
 
-var arr = ["Let Us C Solutions","Password Strength Checker","Soccer 'C' Game","Equations Solver","Matrix Rank Finder","Chat App (Windows)","Automatic Mouse Alphabet Writer","Chat App (Android)","Tic-Tac-Toe (Android)"];
-var links = ["Let Us C Solutions.zip","Password Strength Checker.zip","Soccer Game.zip","Equations Solver.zip","Matrix Rank Finder.zip","Chat App.zip","alphabetWriter_pyautogui.zip","chat_app_android.zip","tictactoe_android.zip"];
+var arr = ["Let Us C Solutions","Password Strength Checker","Soccer 'C' Game","Equations Solver","Matrix Rank Finder","Chat App (Windows)","Automatic Mouse Alphabet Writer","Chat App (Android)","Tic-Tac-Toe (Android)","IG NotePad"];
+var links = ["Let Us C Solutions.zip","Password Strength Checker.zip","Soccer Game.zip","Equations Solver.zip","Matrix Rank Finder.zip","Chat App.zip","alphabetWriter_pyautogui.zip","chat_app_android.zip","tictactoe_android.zip","igNotePad.zip"];
 
 function check_login_status()
 {
